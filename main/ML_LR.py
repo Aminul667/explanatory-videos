43cf658ed9d42a22d9cfb0d4973942922edc779c
Aminul667 <rahathssn@gmail.com>
2024-04-15 01:00:50 +0600

    added colors to the column labels

--- main/ML_LR.py
+++ main/ML_LR.py
@@ -12,7 +12,14 @@ class TableExamples(Scene):
             data,
             # row_labels=[Text("R1"), Text("R2")],
             col_labels=[Text("Size"), Text("Price")],
-            include_outer_lines=True)
+            include_outer_lines=True,
+            line_config={"stroke_width": 1, "color": YELLOW}
+        )
+
+        col_labs = table.get_col_labels()
+        
+        col_labs.set_color(RED)
         table.scale(0.5)
+
         self.play(FadeIn(table), run_time=3)
         self.wait()
