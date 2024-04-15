1b0636cb851beb4f8b2b352846b177ca87b8a3ca
Aminul667 <rahathssn@gmail.com>
2024-04-16 01:58:09 +0600

    added graph

--- main/ML_LR.py
+++ main/ML_LR.py
@@ -14,7 +14,6 @@ class TableExamples(Scene):
             ["1800", "35000", "", ""]
         ]
 
-        
         table = Table(
             data,
             # row_labels=[Text("R1"), Text("R2")],
@@ -25,7 +24,8 @@ class TableExamples(Scene):
         tablePredict = Table(
             dataPredict,
             # row_labels=[Text("R1"), Text("R2")],
-            col_labels=[Text("Size"), Text("Price"), Text("Predicted Price"), Text("Actual-Predicted")],
+            col_labels=[Text("Size"), Text("Price"), Text(
+                "Predicted Price"), Text("Actual-Predicted")],
             include_outer_lines=True,
             line_config={"stroke_width": 1, "color": YELLOW}
         )
@@ -36,10 +36,10 @@ class TableExamples(Scene):
         col_labs_predict = tablePredict.get_col_labels()
 
         col_labs.set_color(RED)
-        table.scale(0.4).to_edge(LEFT)
+        table.scale(0.4).to_edge(UL)
 
         col_labs_predict.set_color(RED)
-        tablePredict.scale(0.4).to_edge(LEFT)
+        tablePredict.scale(0.4).to_edge(UL)
 
         self.play(Write(table), run_time=3)
         self.wait()
@@ -48,3 +48,23 @@ class TableExamples(Scene):
         self.play(Write(tablePredict), run_time=3)
         self.remove(table)
         self.wait()
+
+        ax = Axes(
+            x_range=[0, 2000, 200],
+            y_range=[0, 40000, 4000],
+            x_length=10,
+            # y_length=10,
+            tips=False,
+            axis_config={"include_numbers": True},
+            y_axis_config={"include_numbers": True},
+        )
+        ax.scale(0.6).move_to(DOWN)
+
+        dot_1 = Dot(ax.c2p(1200, 25000), color=GREEN)
+        dot_2 = Dot(ax.c2p(1500, 30000), color=GREEN)
+        dot_3 = Dot(ax.c2p(1800, 35000), color=GREEN)
+
+        graph = ax.plot(lambda x: 20*x, x_range=[0.001, 1800], use_smoothing=False)
+
+        self.add(ax, dot_1, dot_2, dot_3, graph)
+
